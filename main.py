368bbe13d590e8c727e0197981c41c3942301456
Pablo Arrieta <pablou2015@hotmail.com>
2018-10-28 22:38:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,34 @@
+from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import StandardScaler
 from random import randrange
-from time import time
 import sklearn.metrics as m
-import pandas as pd
-import numpy as np
 
-initial = time()
-dataSet = pd.read_csv("wdbc.txt",delimiter=',')
-
-X, y = dataSet.values[:,2:], dataSet.values[:,1]
-np.place(y, y=='M',1)
-np.place(y, y=='B',0)
-y=y.astype('int')
-
+dataset = load_breast_cancer()
+X = dataset.data
+y = dataset.target
+c = [randrange(1, 1000) for i in range(20) ]
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.30,random_state=0, stratify=y)
-c = [randrange(1, 10000, 100)/float(1000) for i in range(100) ]
-c = list(set(c))
+X_train_Nor = StandardScaler().fit_transform(X_train[:])
+X_test_Nor = StandardScaler().fit_transform(X_test[:])
 
-Logis1 = [LogisticRegression(C=i, solver="lbfgs", max_iter=1000).fit(X_train,y_train) for i in c]
-cros = [cross_val_score(i,X_train,y_train,cv=10).mean() for i in Logis1]
-max_cros = max(cros)
-index = [i for i,x in enumerate(cros) if x==max_cros]
-LR = LogisticRegression(C=c[index[0]], solver="lbfgs", max_iter=1000).fit(X_train,y_train)
-Model = LR.predict(X_test)
+svm = [SVC(C=i,gamma='scale').fit(X_train_Nor,y_train) for i in c]
+cros = [cross_val_score(i,X_train_Nor,y_train,cv=10,n_jobs=-1).mean() for i in svm]
 
-f1 = m.f1_score(y_test,Model)
-recall = m.recall_score(y_test,Model)
-accuracy = m.accuracy_score(y_test,Model)
-precision = m.precision_score(y_test,Model)
-tn, fp, fn, tp = m.confusion_matrix(y_test,Model).ravel()
+svm_f = SVC(C=cros[cros.index(max(cros))],gamma='scale').fit(X_train_Nor,y_train)
+model = svm_f.predict(X_test_Nor) 
+
+f1 = m.f1_score(y_test,model)
+recall = m.recall_score(y_test,model)
+accuracy = m.accuracy_score(y_test,model)
+precision = m.precision_score(y_test,model)
+tn, fp, fn, tp = m.confusion_matrix(y_test,model).ravel()
 specificity = tn/float(tn+fp)
-final = time()
+
 print("\n\t   CONFUSION MATRIX")
 print("         Negative     Positive")
 print("Negative   {0}           {1}".format(tn,fp))
-print("Positive   {0}            {1}".format(fn,tp))
+print("Positive   {0}           {1}".format(fn,tp))
 print("\nF1-score: {0}\nRecall: {1}\nAccuracy: {2}\nPrecision: {3}\nSpecificity: {4}".format(f1,recall,accuracy,precision,specificity))
-print("\nExecution time: "+str(round(final-initial,2))+" seconds")
